9eaf1cfb355aa4f0b6e977187dfe7ee108fb6009
ninegravity10 <kishan@ninegravity.com>
2024-03-05 13:09:47 +0530

    Add JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,89 @@
- // Initialize the datepicker
- $('.datepicker').datepicker({
-     format: 'mm/dd/yyyy',
-     autoclose: true
- });
+// Initialize the datepicker
+$('.datepicker').datepicker({
+    format: 'mm/dd/yyyy',
+    autoclose: true
+});
 
-  // Initialize the timepicker
- $(document).ready(function () {
-     $('#timepicker').timepicker({
-         showMeridian: false,
-         minuteStep: 1,
-         defaultTime: '12:00 AM'
-     });
+// Initialize the timepicker
+$(document).ready(function () {
+    $('#timepicker').timepicker({
+        showMeridian: false,
+        minuteStep: 1,
+        defaultTime: '12:00 AM'
+    });
 
-     $('#toggle-btn').on('click', function () {
-         $('#timepicker').timepicker('show');
-     });
- });
+    $('#toggle-btn').on('click', function () {
+        $('#timepicker').timepicker('show');
+    });
+});
+
+
+// Show/Hide Elemets
+function toggleTechElements() {
+    var techFields = document.querySelector('.section_names');
+    if (techFields.style.display === 'none') {
+        techFields.style.display = 'block';
+    } else {
+        techFields.style.display = 'none';
+    }
+}
+
+// Show/Hide Tech. Fields
+function toggleTechFields() {
+    var techFields = document.querySelector('.techFields');
+    if (techFields.style.display === 'none') {
+        techFields.style.display = 'block';
+    } else {
+        techFields.style.display = 'none';
+    }
+}
+
+// Show/Hide Tech. Fields for Short Text
+function toggleTechFieldsShort() {
+    var techFields = document.querySelector('.techFieldsShort');
+    if (techFields.style.display === 'none') {
+        techFields.style.display = 'block';
+    } else {
+        techFields.style.display = 'none';
+    }
+}
+
+// Show/Hide Tech. Fields for long Text
+function toggleTechFieldsLong() {
+    var techFields = document.querySelector('.techFieldsLong');
+    if (techFields.style.display === 'none') {
+        techFields.style.display = 'block';
+    } else {
+        techFields.style.display = 'none';
+    }
+}
+
+// Show/Hide Tech. Fields for Numbers
+function toggleTechFieldsNumbers() {
+    var techFields = document.querySelector('.techFieldsNumbers');
+    if (techFields.style.display === 'none') {
+        techFields.style.display = 'block';
+    } else {
+        techFields.style.display = 'none';
+    }
+}
+
+// Show/Hide Tech. Fields for Date
+function toggleTechFieldsDate() {
+    var techFields = document.querySelector('.techFieldsDate');
+    if (techFields.style.display === 'none') {
+        techFields.style.display = 'block';
+    } else {
+        techFields.style.display = 'none';
+    }
+}
+
+// Show/Hide Tech. Fields for Time
+function toggleTechFieldsTime() {
+    var techFields = document.querySelector('.techFieldsTime');
+    if (techFields.style.display === 'none') {
+        techFields.style.display = 'block';
+    } else {
+        techFields.style.display = 'none';
+    }
+}
